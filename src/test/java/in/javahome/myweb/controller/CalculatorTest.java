223f35308c76b14879e40852e51a4022a9edce67
mndillon <44403324+mndillon@users.noreply.github.com>
2019-03-28 21:41:15 +0000

    Update CalculatorTest.java

--- src/test/java/in/javahome/myweb/controller/CalculatorTest.java
+++ src/test/java/in/javahome/myweb/controller/CalculatorTest.java
@@ -13,38 +13,7 @@ public class CalculatorTest extends TestCase {
 	Calculator cal = new Calculator();
 
 	public void testAdd() {
-		Properties prop = new Properties();
-		
-		OutputStream output = null;
-
-		try
-		{
-
-			output = new FileOutputStream("../config.properties");
-
-			// set the properties value
-			prop.setProperty("database", "localhost");
-			prop.setProperty("dbuser", "hari");
-			prop.setProperty("dbpassword", "password");
-
-			// save properties to project root folder
-			prop.store(output, null);
-
-		}catch(
-		IOException io)
-		{
-			io.printStackTrace();
-		}finally
-		{
-			if (output != null) {
-				try {
-					output.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-
-		}
+		Assert.assertEquals(cal.add(10, 20), 30);
 	}
 
 	public void testMultiply() {
