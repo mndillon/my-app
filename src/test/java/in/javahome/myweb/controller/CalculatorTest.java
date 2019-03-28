abe473835948c4501dbd9fa81f54b5c6a31cb273
mndillon <44403324+mndillon@users.noreply.github.com>
2019-03-28 21:56:58 +0000

    Update CalculatorTest.java

--- src/test/java/in/javahome/myweb/controller/CalculatorTest.java
+++ src/test/java/in/javahome/myweb/controller/CalculatorTest.java
@@ -6,15 +6,17 @@ import static org.junit.Assert.assertEquals;
 public class CalculatorTest  {
 	
 
-	@Test
-	public void testAdd() {
-		Calculator cal = new Calculator();
-		assertEquals(cal.add(10, 20), 30);
-	}
+	
 
 	@Test
 	public void testMultiply() {
 		Calculator cal = new Calculator();
 		assertEquals(cal.multiply(10, 20), 200);
+	}
+	
+	@Test
+	public void testAdd() {
+		Calculator cal = new Calculator();
+		assertEquals(cal.add(10, 20), 30);
 	}
 }
