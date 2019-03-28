9e688cfd53c0e95463387bfa9b2698dd89ae614a
mndillon <44403324+mndillon@users.noreply.github.com>
2019-03-28 21:46:03 +0000

    Update CalculatorTest.java

--- src/test/java/in/javahome/myweb/controller/CalculatorTest.java
+++ src/test/java/in/javahome/myweb/controller/CalculatorTest.java
@@ -1,22 +1,18 @@
 package in.javahome.myweb.controller;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Properties;
-
-import junit.framework.Assert;
-import junit.framework.TestCase;
-
-public class CalculatorTest extends TestCase {
+public class CalculatorTest  {
 	Calculator cal = new Calculator();
 
+	@Test
 	public void testAdd() {
-		Assert.assertEquals(cal.add(10, 20), 30);
+		assertEquals(cal.add(10, 20), 30);
 	}
 
+	@Test
 	public void testMultiply() {
-		Assert.assertEquals(cal.multiply(10, 20), 200);
+		assertEquals(cal.multiply(10, 20), 200);
 	}
 }
